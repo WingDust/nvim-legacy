cd71cb8ccb32794a0714e89164a4f851adec86d6
wingdust <995315823@qq.com>
2022-06-03 09:09:37 +0800

    Featur: 支持 windows10 p 直接粘贴 clipboard

--- init.lua
+++ init.lua
@@ -1,19 +1,44 @@
 require("plugins")
+require("keybindings")
 
+
+--vim.api.nvim_eval('set shell=pwsh')
+--
+-- lua config
+-- https://stackoverflow.com/questions/36108950/setting-up-powershell-as-vims-shell-command-does-not-seem-to-be-passed-correct
+vim.cmd([[
+set shell=pwsh
+set shellcmdflag=-command
+set shellquote=\"
+set shellxquote=
+set clipboard=unnamed
+]])
 
 function c()
   print(222)
 end
 -- treesitter
 --  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+--
+
 vim.api.nvim_create_user_command(
   'Ad', 
   function(opts) 
-    vim.pretty_print(vim.api.nvim_win_get_cursor(0)) 
-    p = vim.treesitter.get_parser(0)
-    vim.pretty_print(p._lang) 
+    --vim.pretty_print(vim.api.nvim_win_get_cursor(0)) 
+    --p = vim.treesitter.get_parser(0)
+    --vim.pretty_print(p._lang) 
+    --vim.pretty_print(p._lang) 
+    --vim.pretty_print(vim.fn.expand('%'))
+    --vim.pretty_print(type(vim.api.nvim_buf_get_name(0)))
 
-    --vim.pretty_print(opts.fargs) 
+    cmd = ("node tree-sitter.js"..vim.api.nvim_buf_get_name(0))
+
+   -- os.execute(cmd)
+    
+    --vim.fn.expand('%')
+
+   vim.pretty_print(opts.fargs) 
+   --vim.pretty_print(opts) 
   end, 
   { nargs = '*' }
   )
